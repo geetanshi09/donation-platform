095e2caa38d67fb01b02611bce67762fe625c7ff
geetanshi <geetanshisingla@gmail.com>
2025-09-16 20:45:47 +0530

    Initial structure

---
+++ src/test/java/com/donation/donation_platform/DonationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donation.donation_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
